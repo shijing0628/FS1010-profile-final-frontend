7116c9f9bf3a58395f32366e3d0c520c65f3e1ee
shijing0628 <brendaliu0628@gmail.com>
2021-02-24 14:43:09 -0600

    add project detail page

--- src/components/SocialFollow/SocialFollow.js
+++ src/components/SocialFollow/SocialFollow.js
@@ -13,19 +13,19 @@ export default function SocialFollow() {
   return (
     <div className="social-container">
       <a href="https://www.youtube.com" className="youtube social">
-        <FontAwesomeIcon icon={faYoutube} size="2x" />
+        <FontAwesomeIcon icon={faYoutube} />
       </a>
       <a href="https://www.facebook.com" className="facebook social">
-        <FontAwesomeIcon icon={faFacebook} size="2x" />
+        <FontAwesomeIcon icon={faFacebook} />
       </a>
       <a href="https://www.linkedin" className="linkedin social">
-        <FontAwesomeIcon icon={faLinkedin} size="2x" />
+        <FontAwesomeIcon icon={faLinkedin} />
       </a>
       <a href="https://www.twitter.com" className="twitter social">
-        <FontAwesomeIcon icon={faTwitter} size="2x" />
+        <FontAwesomeIcon icon={faTwitter} />
       </a>
       <a href="https://www.instagram.com" className="instagram social">
-        <FontAwesomeIcon icon={faInstagram} size="2x" />
+        <FontAwesomeIcon icon={faInstagram} />
       </a>
     </div>
   );
